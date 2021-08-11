6243c72a519978503673810a91a259f2f0ee47f5
Barasher <barasher@nc10.lan>
2021-08-11 23:59:45 +0200

    Add an example for metadata writing

--- exiftool_sample_test.go
+++ exiftool_sample_test.go
@@ -2,11 +2,15 @@ package exiftool_test
 
 import (
 	"fmt"
+	"io"
+	"os"
+
+	"path/filepath"
 
 	"github.com/barasher/go-exiftool"
 )
 
-func ExampleExiftool() {
+func ExampleExiftool_Read() {
 	et, err := exiftool.NewExiftool()
 	if err != nil {
 		fmt.Printf("Error when intializing: %v\n", err)
@@ -26,4 +30,49 @@ func ExampleExiftool() {
 			fmt.Printf("[%v] %v\n", k, v)
 		}
 	}
+}
+
+func copyFile(src, dest string) (err error) {
+	s, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+
+	d, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+
+	_, err = io.Copy(d, s)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ExampleExiftool_Write() {
+	// error handling are skipped in this example
+
+	tmpDir, _ := os.MkdirTemp("", "ExampleExiftoolWrite")
+	testFile := filepath.Join(tmpDir, "20190404_131804.jpg")
+	copyFile("testdata/20190404_131804.jpg", testFile)
+
+	e, _ := exiftool.NewExiftool()
+	defer e.Close()
+	originals := e.ExtractMetadata(testFile)
+	title, _ := originals[0].GetString("Title")
+	fmt.Println("title:" + title)
+
+	originals[0].SetString("Title", "newTitle")
+	e.WriteMetadata(originals)
+
+	altered := e.ExtractMetadata(testFile)
+	title, _ = altered[0].GetString("Title")
+	fmt.Println("title:" + title)
+
+	// Output:
+	// title:
+	// title:newTitle
 }
